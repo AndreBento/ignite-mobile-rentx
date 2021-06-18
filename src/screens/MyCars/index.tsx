6ea6e8d02d781b8a007b44ad43d65a28230cc0e3
AndreBento <bento.cab@gmail.com>
2021-06-18 03:31:41 -0300

    Reaproveitando o Componente Car

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -2,7 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { FlatList, StatusBar } from 'react-native';
 import { useTheme } from 'styled-components';
+import { AntDesign } from '@expo/vector-icons';
+
 import { BackButton } from '../../components/BackButton';
+import { Load } from '../../components/Load';
 
 import { Car } from '../../components/Car';
 import { CarDTO } from '../../dtos/CarDTO';
@@ -17,12 +20,19 @@ import {
     Appointments,
     AppointmentsTitle,
     AppointmentsQuantity,
+    CarWrapper,
+    CarFooter,
+    CarFooterTitle,
+    CarFooterPeriod,
+    CarFooterDate,
 } from './styles';
 
 interface CarProps {
     id: string;
     user_id: string;
     car: CarDTO;
+    startDate: string;
+    endDate: string;
 }
 
 export function MyCars() {
@@ -69,20 +79,48 @@ export function MyCars() {
 
                 <SubTitle>Conforto, segurança e praticidade.</SubTitle>
             </Header>
+            {loading ? (
+                <Load />
+            ) : (
+                <Content>
+                    <Appointments>
+                        <AppointmentsTitle>
+                            Agendamentos fitos
+                        </AppointmentsTitle>
+                        <AppointmentsQuantity>
+                            {cars.length}
+                        </AppointmentsQuantity>
+                    </Appointments>
 
-            <Content>
-                <Appointments>
-                    <AppointmentsTitle>Agendamentos fitos</AppointmentsTitle>
-                    <AppointmentsQuantity>5</AppointmentsQuantity>
-                </Appointments>
-
-                <FlatList
-                    data={cars}
-                    keyExtractor={item => item.id}
-                    showsVerticalScrollIndicator={false}
-                    renderItem={({ item }) => <Car data={item.car} />}
-                />
-            </Content>
+                    <FlatList
+                        data={cars}
+                        keyExtractor={item => item.id}
+                        showsVerticalScrollIndicator={false}
+                        renderItem={({ item }) => (
+                            <CarWrapper>
+                                <Car data={item.car} />
+                                <CarFooter>
+                                    <CarFooterTitle>Período</CarFooterTitle>
+                                    <CarFooterPeriod>
+                                        <CarFooterDate>
+                                            {item.startDate}
+                                        </CarFooterDate>
+                                        <AntDesign
+                                            name="arrowright"
+                                            size={20}
+                                            color={theme.colors.title}
+                                            style={{ marginHorizontal: 10 }}
+                                        />
+                                        <CarFooterDate>
+                                            {item.endDate}
+                                        </CarFooterDate>
+                                    </CarFooterPeriod>
+                                </CarFooter>
+                            </CarWrapper>
+                        )}
+                    />
+                </Content>
+            )}
         </Container>
     );
 }
--- src/screens/SchedulingDatails/index.tsx
+++ src/screens/SchedulingDatails/index.tsx
@@ -76,6 +76,14 @@ export function SchedulingDatails() {
         await api.post('schedules_byuser', {
             user_id: 1,
             car,
+            startDate: format(
+                getPlatformDate(new Date(dates[0])),
+                'dd/MM/yyyy',
+            ),
+            endDate: format(
+                getPlatformDate(new Date(dates[dates.length - 1])),
+                'dd/MM/yyyy',
+            ),
         });
 
         api.put(`/schedules_bycars/${car.id}`, {
